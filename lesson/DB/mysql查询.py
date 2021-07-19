a40b9706680e44819eb07e966e883aef910a16da
sandy1988302 <70570253+sandy1988302@users.noreply.github.com>
2021-07-19 10:59:05 +0800

    update

---
+++ lesson/DB/mysql查询.py
@@ -0,0 +1,29 @@
+import pymysql
+
+# 打开数据库连接
+db = pymysql.connect(host="localhost", user="root", password="1qaz@WSX", database="mysql")
+
+# 使用cursor()方法获取操作游标
+cursor = db.cursor()
+
+# SQL 查询语句
+sql = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > %s" % 1000
+try:
+    # 执行SQL语句
+    cursor.execute(sql)
+    # 获取所有记录列表
+    results = cursor.fetchall()
+    for row in results:
+        firstname = row[0]
+        lastname = row[1]
+        age = row[2]
+        sex = row[3]
+        income = row[4]
+        # 打印结果
+        print("firstname=%s,lastname=%s,age=%s,sex=%s,income=%s" % (firstname, lastname, age, sex, income))
+except:
+    print("Error: unable to fetch data")
+
+# 关闭数据库连接
+db.close()
